277cd13a3eff12d8bf6ce1169d1406cd0557a30d
Agustin Ferreira <agustinn1233@hotmail.com>
2021-01-10 14:53:54 -0300

    Maps
    Google maps implementation.
    
    Best practices by google.
    
    Code clean.

---
+++ app/src/main/java/com/github/agustinf1233/mymapsapp/MapsActivity.kt
@@ -0,0 +1,43 @@
+package com.github.agustinf1233.mymapsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Uruguay, Montevideo.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val uruguay = LatLng(-34.905720, -56.163660)
+        mMap.addMarker(MarkerOptions().position(uruguay).title("Marker in Montevideo, Uruguay"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(uruguay))
+    }
+}
